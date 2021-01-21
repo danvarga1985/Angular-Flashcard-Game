c791722c4d4ebf571fa5da9ddffa6b32105b02ec
danvarga1985 <danvarga1985@gmail.com>
2021-01-21 13:24:05 +0100

    Adding placeholder data, implementing logic, formatting style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IFlash } from './model/flash.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  editing = false;
+  editingId: number;
+
   title = 'angular-flashcard-game';
+
+  flashs: IFlash[] = [
+    {
+      question: 'Question 1',
+      answer: 'Answer 1',
+      show: false,
+      id: getRandomNumber(),
+    },
+    {
+
+      question: 'Question 2',
+      answer: 'Answer 2',
+      show: false,
+      id: getRandomNumber(),
+    },
+    {
+      question: 'Question 3',
+      answer: 'Answer 3',
+      show: false,
+      id: getRandomNumber(),
+    }
+  ];
+
+  trackByFlashId(index: number, flash: IFlash): number {
+    return flash.id;
+  }
+
+  handleToggleCard(id: number): void {
+    const flash: IFlash = this.flashs.find(flash => flash.id === id);
+
+    flash.show = !flash.show;
+  }
+  
+  handleDelete(id: number): void {
+    const flashId: number = this.flashs.indexOf(flash => flash.id === id);
+    this.flashs.splice(flashId, 1);
+  }
+  
+  handleEdit(id: number): void {
+    this.editing = true;
+    this.editingId = id;
+    // TODO: add editing locig after adding the form
+  }
+  
+  handleRememberedChange({id, flag}): void {
+    const flash: IFlash = this.flashs.find(flash => flash.id === id);
+    flash.remembered = flag === 'correct' || flag === 'incorrect' ? flag : null;
+  }
+}
+
+function getRandomNumber(): number {
+  return Math.floor(Math.random() * 10000);
 }
--- src/app/flash/flash.component.ts
+++ src/app/flash/flash.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { IFlash } from '../model/flash.model';
 
 @Component({
@@ -7,6 +7,10 @@ import { IFlash } from '../model/flash.model';
   styleUrls: ['./flash.component.css']
 })
 export class FlashComponent implements OnInit {
+  @Output() onToggleCard = new EventEmitter();
+  @Output() onDelete = new EventEmitter();
+  @Output() onEdit = new EventEmitter();
+  @Output() onRememberedChange = new EventEmitter();
 
   @Input() flash: IFlash = {
     id: 1,
@@ -18,6 +22,32 @@ export class FlashComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleCard(): void {
+    this.onToggleCard.emit(this.flash.id);
+  }
+
+  deleteFlash(): void {
+    this.onDelete.emit(this.flash.id);
+  }
+  
+  editFlash(): void {
+    this.onEdit.emit(this.flash.id);
+  }
+  
+  markCorrect(): void {
+    this.onRememberedChange.emit({
+      id: this.flash.id,
+      flag: 'correct',
+    });
+  }
+  
+  markIncorrect(): void {
+    this.onRememberedChange.emit({
+      id: this.flash.id,
+      flag: 'incorrect',
+    });
   }
 
 }
